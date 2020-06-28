f6642b40eeb247476abf0bd86fb8181047c28c67
Yehuda Volodin <falcon81@tut.by>
2020-06-28 22:56:28 +0300

    Updates

---
+++ precache-manifest.0078d7da555713f10dec5b2c474eea3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afadade48edb5de95663db33fd385f20",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/index.html"
+  },
+  {
+    "revision": "02ed6680c4717277dcd2",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "466c9e9fe961c3f7a6c7",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/css/main.84803e67.chunk.css"
+  },
+  {
+    "revision": "02ed6680c4717277dcd2",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/js/2.43b26026.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/js/2.43b26026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466c9e9fe961c3f7a6c7",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/js/main.94f1939b.chunk.js"
+  },
+  {
+    "revision": "d6309694695caa410956",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/js/runtime-main.6c534727.js"
+  },
+  {
+    "revision": "257009bb7b307af9e2bc1af543bdf241",
+    "url": "/yehuda-volodin-contact-manager-using-Redux/static/media/default_bg.257009bb.jpg"
+  }
+]);
